213cda512422ba23fded96e6567027335cdbc5c8
root <wassim.mussard@etu.u-pec.fr>
2025-09-30 14:09:48 +0200

    Deploy: 2025-09-30 14:09:48

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759234132',resources: [
+    cacheName: 'chirpy-1759234187',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
